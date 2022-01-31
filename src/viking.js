0f1e9c74f79bfd71ab0be002f789e3b50295ece6
Noelia Parada <noeliacastrelo@gmail.com>
2022-01-31 17:25:51 +0100

    a ver si tengo tiempo después a hacer los bonus

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,60 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(healthParam, strengthParam) {
+    this.health = healthParam;
+    this.strength = strengthParam;
+  }
+    attack () {
+    return this.strength;
+    }
+
+    receiveDamage (damageParam){
+    this.health = this.health - damageParam;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking  extends Soldier {
+  constructor(nameParam, healthParam, strengthParam) {
+    super(healthParam, strengthParam);
+    this.name = nameParam;
+  }
+
+  receiveDamage (damageParam){
+    this.health = this.health - damageParam;
+
+    if(this.health > 0){
+    return `${this.name} has received ${damageParam} points of damage`;
+    }else{
+    return `${this.name} has died in act of combat`;
+  }
+}
+
+battleCry(){
+  return "Odin Owns You All!";
+}
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+ constructor(healthParam, strengthParam) {
+   super(healthParam, strengthParam);
+ }
+
+receiveDamage (damageParam){
+  this.health = this.health - damageParam;
+
+  if(this.health > 0){
+    return `A Saxon has received ${damageParam} points of damage`;
+    }else{
+    return "A Saxon has died in combat";
+  }
+}
+}
 
 // War
-class War {}
+class War{}
 
 
 
